b7a4c3cac23b74ee4ff2595765545bbdd512e488
Tim Beals <tim.beals@gmail.com>
2017-09-23 10:52:14 -0700

    create project

---
+++ Alarmadillo/Alarmadillo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alarmadillo
+//
+//  Created by Tim Beals on 2017-09-23.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
